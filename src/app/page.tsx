ae707f8c9618fbd30a0747e2925854dfadebf516
devWillianFerreira <ferreiraw36@gmail.com>
2025-08-15 18:01:46 -0300

    feat: add homepage desktop banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { desc } from "drizzle-orm";
-import Image from "next/image";
+import Image, { getImageProps } from "next/image";
 
 import CategoryProduct from "@/components/commom/category-product";
 import Footer from "@/components/commom/footer";
 import Header from "@/components/commom/header";
+import NavigationBar from "@/components/commom/navigation-bar";
 import PartnerBrands from "@/components/commom/partner-brands";
 import ProductList from "@/components/commom/product-list";
 import { db } from "@/db";
@@ -24,25 +25,57 @@ export default async function Home() {
   });
 
   const category = await db.query.categoryTable.findMany({});
+
+  const common = { alt: "Leve uma vida com estilo", sizes: "100vw" };
+  const {
+    props: { srcSet: desktop },
+  } = getImageProps({
+    ...common,
+    width: 0,
+    height: 0,
+    quality: 80,
+    src: "/banner-01-desktop.png",
+  });
+  const {
+    props: { srcSet: mobile, ...rest },
+  } = getImageProps({
+    ...common,
+    width: 0,
+    height: 0,
+    quality: 70,
+    src: "/banner-01.png",
+  });
   return (
     <>
       <Header />
+      <NavigationBar />
       <div className="space-y-6">
         <div className="px-5">
-          <Image
-            src="/banner-01.png"
-            alt="Leve uma vida com estilo"
-            height={0}
-            width={0}
-            sizes="100vw"
-            className="h-auto w-full"
-          />
+          <picture>
+            <source
+              media="(min-width: 768px)"
+              srcSet={desktop}
+              sizes="100vw"
+              className="h-auto w-full"
+            />
+            <source
+              media="(min-width: 500px)"
+              srcSet={mobile}
+              sizes="100vw"
+              className="h-auto w-full"
+            />
+            <img
+              {...rest}
+              style={{ width: "100%", height: "auto" }}
+              alt={rest.alt}
+            />
+          </picture>
         </div>
         <PartnerBrands />
 
         <ProductList products={products} title="Mais vendidos" />
 
-        <div className="px-5">
+        <div className="px-5 md:hidden">
           <CategoryProduct category={category} />
         </div>
 
--- src/components/commom/product-list.tsx
+++ src/components/commom/product-list.tsx
@@ -13,9 +13,9 @@ interface ProductListProps {
 
 const ProductList = ({ title, products }: ProductListProps) => {
   return (
-    <div className="space-y-6">
+    <div className="space-y-6 xl:px-5">
       <h3 className="px-5 font-semibold">{title}</h3>
-      <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+      <div className="flex w-full flex-1 gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
         {products.map((product) => (
           <ProductItem key={product.id} product={product} />
         ))}
