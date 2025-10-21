5216d4d8fe0676ee57d6c60fcbca634bca0bfb05
devWillianFerreira <ferreiraw36@gmail.com>
2025-10-21 20:41:44 -0300

    feat: add cartTable query to identification page

--- src/app/cart/identification/page.tsx
+++ src/app/cart/identification/page.tsx
@@ -1,14 +1,71 @@
-import OrderSteps from "../components/order-steps";
-import Adress from "./components/address";
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const Identification = () => {
+import { db } from "@/db";
+import { cartTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
+
+import OrderSteps from "../components/order-steps";
+import Address from "./components/address";
+import YourOrders from "./components/your-orders";
+
+const Identification = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user.id) {
+    redirect("/");
+  }
+
+  const cart = await db.query.cartTable.findFirst({
+    where: eq(cartTable.userId, session.user.id),
+    with: {
+      shippingAddress: true,
+      items: {
+        with: {
+          productVariant: {
+            with: {
+              product: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (!cart || cart.items.length == 0) {
+    redirect("/");
+  }
+
+  const cartTotalInCents = cart.items.reduce(
+    (acc, item) => acc + item.productVariant.priceInCents * item.quantity,
+    0,
+  );
+
   return (
     <div className="space-y-5 px-5">
       <OrderSteps hasIdentification={true} hasPayment={false} />
-      <div className="grid grid-cols-1 lg:grid-cols-[s2fr_1fr]">
+      <div className="grid grid-cols-1 items-start gap-5 lg:grid-cols-[2fr_1fr]">
         <div className="space-y-6 rounded-lg border p-3">
           <h1 className="text-xl font-semibold">Identificação</h1>
-          <Adress />
+          <Address />
+        </div>
+
+        <div className="h-auto rounded-lg border p-4">
+          <h1 className="text-xl font-bold">Seu Pedido</h1>
+          <YourOrders
+            subTotalPriceInCents={cartTotalInCents}
+            totalPriceInCents={cartTotalInCents}
+            products={cart.items.map((item) => ({
+              id: item.id,
+              imageUrl: item.productVariant.imageUrl,
+              name: item.productVariant.product?.name ?? "",
+              variantName: item.productVariant.name,
+              priceInCents: item.productVariant.priceInCents,
+            }))}
+          />
         </div>
       </div>
     </div>
