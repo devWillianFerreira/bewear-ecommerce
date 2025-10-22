17fa658b96546484253b5226c169f695c7c48a5a
devWillianFerreira <ferreiraw36@gmail.com>
2025-10-22 20:05:11 -0300

    feat: ensure user addresses are shown in identification page

---
+++ src/actions/get-address/index.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { toast } from "sonner";
+
+import { db } from "@/db";
+import { shippingAddressTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
+
+const getAddress = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    throw new Error("unauthorized");
+  }
+
+  try {
+    const address = await db.query.shippingAddressTable.findFirst({
+      where: eq(shippingAddressTable.userId, session.user.id),
+    });
+
+    return address;
+  } catch (error) {
+    toast.error("Erro ao buscar os endereços");
+    console.log("Erro ao buscar endereço:", error);
+  }
+};
+
+export default getAddress;
