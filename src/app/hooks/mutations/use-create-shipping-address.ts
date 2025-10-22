7c98eb48d927ff1a2f517c2012f8d3a25d92608b
devWillianFerreira <ferreiraw36@gmail.com>
2025-10-22 20:30:37 -0300

    featL add mutation to create address

---
+++ src/app/hooks/mutations/use-create-shipping-address.ts
@@ -0,0 +1,21 @@
+import { QueryClient, useMutation } from "@tanstack/react-query";
+
+import createShippingAddress from "@/actions/create-shipping-address";
+
+import { getUserAdressQuerieKey } from "../queries/use-shipping-address";
+
+export const getCreateShippingAddressMutationKey = () =>
+  ["create-shipping-address"] as const;
+
+export function useCreateShippingAddress() {
+  const queryClient = new QueryClient();
+  return useMutation({
+    mutationKey: getCreateShippingAddressMutationKey(),
+    mutationFn: createShippingAddress,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: getUserAdressQuerieKey(),
+      });
+    },
+  });
+}
