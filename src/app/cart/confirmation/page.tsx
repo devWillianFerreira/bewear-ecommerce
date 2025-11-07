2706b506a40eea7c2386666ed0e32cb85a0f15e2
devWillianFerreira <ferreiraw36@gmail.com>
2025-11-06 21:52:57 -0300

    feat: add confirmation page

--- src/app/cart/confirmation/page.tsx
+++ src/app/cart/confirmation/page.tsx
@@ -1,23 +1,31 @@
 import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
 import { db } from "@/db";
-import { cartTable, shippingAddressTable } from "@/db/schema";
+import { cartTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import OrderSteps from "../components/order-steps";
 import YourOrders from "../components/your-orders";
-import Address from "./components/address";
+import { formatAddress } from "../helpers/address";
 
-const Identification = async () => {
+const Confirmation = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
-  if (!session?.user.id) {
+
+  if (!session?.user) {
     redirect("/");
   }
-
   const cart = await db.query.cartTable.findFirst({
     where: eq(cartTable.userId, session.user.id),
     with: {
@@ -34,7 +42,11 @@ const Identification = async () => {
     },
   });
 
-  if (!cart || cart.items.length == 0) {
+  if (!cart) {
+    redirect("/");
+  }
+
+  if (!cart.shippingAddress) {
     redirect("/");
   }
 
@@ -42,22 +54,29 @@ const Identification = async () => {
     (acc, item) => acc + item.productVariant.priceInCents * item.quantity,
     0,
   );
-  const shippingAdress = await db.query.shippingAddressTable.findMany({
-    where: eq(shippingAddressTable.userId, session.user.id),
-  });
-
   return (
-    <div className="space-y-5 px-5">
-      <OrderSteps hasIdentification={true} hasPayment={false} />
-      <div className="grid grid-cols-1 items-start gap-5 lg:grid-cols-[2fr_1fr]">
-        <div className="space-y-6 rounded-lg border p-3">
-          <h1 className="text-xl font-semibold">Identificação</h1>
-          <Address
-            shippingAdresses={shippingAdress}
-            defaultshippingAdressId={cart.shippingAddress?.id || null}
-          />
+    <div className="space-y-5">
+      <OrderSteps hasIdentification={true} hasPayment={true} />
+      <div className="grid grid-cols-1 items-start gap-5 px-5 lg:grid-cols-[2fr_1fr]">
+        <div>
+          <Card>
+            <CardHeader>
+              <h1 className="text-xl font-semibold">Pagamento</h1>
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-col items-center justify-between gap-4 rounded-md border p-3 md:flex-row">
+                <p className="text-muted-foreground">Identificação</p>
+                {formatAddress(cart.shippingAddress)}
+                <Link href="/cart/identification" className="underline">
+                  <p>Alterar</p>
+                </Link>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full rounded-full">Finalizar Compra</Button>
+            </CardFooter>
+          </Card>
         </div>
-
         <div className="h-auto rounded-lg border p-4">
           <h1 className="text-xl font-bold">Seu Pedido</h1>
           <YourOrders
@@ -67,8 +86,8 @@ const Identification = async () => {
               id: item.id,
               imageUrl: item.productVariant.imageUrl,
               name: item.productVariant.product?.name ?? "",
-              variantName: item.productVariant.name,
               priceInCents: item.productVariant.priceInCents,
+              variantName: item.productVariant.name,
             }))}
           />
         </div>
@@ -77,4 +96,4 @@ const Identification = async () => {
   );
 };
 
-export default Identification;
+export default Confirmation;
